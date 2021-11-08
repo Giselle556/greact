5bf3565b8a915ad9fb4857c13c43a76f79c75868
Giselle556 <giselleborbolla@yahoo.com>
2021-11-08 03:39:18 -0700

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Foot(){
 return(
 <Footer
   className="example"
-  // copyrights="Coded by Annalisa Bauer"
+
   links={<ul id="footer"><li><a className="grey-text text-lighten-3" href="https://github.com/Giselle556">GitHub</a></li><li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/giselle-borbolla-46805a16a/">LinkedIn</a></li><li><a className="grey-text text-lighten-3" href="mailto:annalisa.bauer1@gmail.com">gisellesmeborbolla@gmail.com</a></li></ul>}
   
 >
